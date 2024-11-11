03200357a948c2550e89e11f8a679e07fe4645bf
hh-hasib <hhhasib01@gmail.com>
2024-11-11 23:33:07 +0600

    project done

--- Piczle/PuzzleTitle.swift
+++ Piczle/PuzzleTitle.swift
@@ -2,7 +2,13 @@
 //  PuzzleTitle.swift
 //  Piczle
 //
-//  Created by Abir Rahman on 11/11/24.
+//  Created by Hasibul Hasan on 11/11/24.
 //
 
 import Foundation
+import UIKit
+
+struct PuzzleTile: Equatable {
+    let image: UIImage?
+    var isSpareTile = false
+}
--- Piczle/PuzzleTitleView.swift
+++ Piczle/PuzzleTitleView.swift
@@ -2,7 +2,33 @@
 //  PuzzleTitleView.swift
 //  Piczle
 //
-//  Created by Abir Rahman on 11/11/24.
+//  Created by Hasibul Hasan on 11/11/24.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct PuzzleTileView: View {
+    let tile: PuzzleTile
+    var body: some View {
+        VStack {
+            if let image = tile.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+            }
+            else {
+                Color.yellow
+            }
+        }
+        .overlay(content: {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color.yellow, lineWidth: 2)
+        })
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+    }
+}
+
+#Preview {
+    PuzzleTileView(tile: PuzzleTile(image: nil, isSpareTile: true))
+}
