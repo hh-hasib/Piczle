edd210d32fc31bde9bfbccc8584e2a3463bcfc15
hh-hasib <hhhasib01@gmail.com>
2024-11-11 23:31:42 +0600

    project done

---
+++ Piczle/PuzzleLoader.swift
@@ -0,0 +1,8 @@
+//
+//  PuzzleLoader.swift
+//  Piczle
+//
+//  Created by Abir Rahman on 11/11/24.
+//
+
+import Foundation
---
+++ Piczle/PuzzleTitle.swift
@@ -0,0 +1,8 @@
+//
+//  PuzzleTitle.swift
+//  Piczle
+//
+//  Created by Abir Rahman on 11/11/24.
+//
+
+import Foundation
