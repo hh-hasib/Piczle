d58a75199b585ceb76e5ced0eaf08c6fab9bbe9f
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-11-11 21:37:42 +0600

    Initial Commit

---
+++ Piczle/PiczleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiczleApp.swift
+//  Piczle
+//
+//  Created by Abir Rahman on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PiczleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
